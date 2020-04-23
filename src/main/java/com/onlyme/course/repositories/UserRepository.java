bc1638f7f45bb2acb05bab00cce5ee2d2d3f62ff
Lucasrb18 <lucasrb18@gmail.com>
2020-04-22 22:48:56 -0300

    JPA repository, dependency injection,database seeding

---
+++ src/main/java/com/onlyme/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.onlyme.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.onlyme.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
