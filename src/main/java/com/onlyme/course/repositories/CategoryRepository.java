8d056c058c644c41c317bfda775aa9d4705723f2
Lucasrb18 <lucasrb18@gmail.com>
2020-04-23 02:05:04 -0300

    Category

---
+++ src/main/java/com/onlyme/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.onlyme.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.onlyme.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
