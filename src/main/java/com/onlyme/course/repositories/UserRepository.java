3f41d93b761c24f78857096c5d7e0bd244b6486d
Lucasrb18 <lucasrb18@gmail.com>
2020-04-22 23:48:33 -0300

    Service Layer, component registration

--- src/main/java/com/onlyme/course/repositories/UserRepository.java
+++ src/main/java/com/onlyme/course/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.onlyme.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.onlyme.course.entities.User;
 
